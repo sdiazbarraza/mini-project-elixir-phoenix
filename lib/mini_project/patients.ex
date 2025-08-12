265b7304d01cfba46d68882634bd9a72fae0b999
Sebastian Diaz <sebastiandesarrolla@gmail.com>
2025-08-12 23:31:29 +0000

    feat: Loaders patients and practitioners

--- lib/mini_project/patients.ex
+++ lib/mini_project/patients.ex
@@ -1,4 +1,10 @@
 defmodule MiniProject.Patients do
+
+  alias MiniProject.Repo
+  alias MiniProject.Patients.Patient
+
+  @api_url "https://fakerapi.it/api/v1/persons?_locale=es_ES&_quantity=100"
+
   @moduledoc """
   The Patients context.
   """
@@ -101,4 +107,43 @@ defmodule MiniProject.Patients do
   def change_patient(%Patient{} = patient, attrs \\ %{}) do
     Patient.changeset(patient, attrs)
   end
+
+  def loader do
+    case HTTPoison.get(@api_url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        body
+        |> Jason.decode!()
+        |> Map.get("data", [])
+        |> Enum.map(&map_person_to_patient/1)
+        |> Enum.each(&insert_patient/1)
+
+        {:ok, "Patients loaded successfully"}
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
+  end
+
+  defp map_person_to_patient(person) do
+    %{
+      first_name: person["firstname"],
+      last_name: person["lastname"],
+      email: person["email"],
+      phone: person["phone"],
+      birthdate: parse_date(person["birthday"])
+    }
+  end
+
+  defp parse_date(date_string) do
+    case Date.from_iso8601(date_string) do
+      {:ok, date} -> date
+      _ -> nil
+    end
+  end
+
+  defp insert_patient(attrs) do
+    %Patient{}
+    |> Patient.changeset(attrs)
+    |> Repo.insert()
+  end 
 end
--- lib/mini_project/practitioners.ex
+++ lib/mini_project/practitioners.ex
@@ -1,4 +1,10 @@
 defmodule MiniProject.Practitioners do
+
+  alias MiniProject.Repo
+  alias MiniProject.Practitioners.Practitioner
+
+  @api_url "https://fakerapi.it/api/v1/persons?_locale=es_ES&_quantity=100"
+
   @moduledoc """
   The Practitioners context.
   """
@@ -100,5 +106,44 @@ defmodule MiniProject.Practitioners do
   """
   def change_practitioner(%Practitioner{} = practitioner, attrs \\ %{}) do
     Practitioner.changeset(practitioner, attrs)
+  end
+
+  def loader do
+    case HTTPoison.get(@api_url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        body
+        |> Jason.decode!()
+        |> Map.get("data", [])
+        |> Enum.map(&map_person_to_practitioner/1)
+        |> Enum.each(&insert_practitioner/1)
+
+        {:ok, "Practitioners loaded successfully"}
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
+  end
+
+  defp map_person_to_practitioner(person) do
+    %{
+      first_name: person["firstname"],
+      last_name: person["lastname"],
+      email: person["email"],
+      phone: person["phone"],
+      birthdate: parse_date(person["birthday"])
+    }
+  end
+
+  defp parse_date(date_string) do
+    case Date.from_iso8601(date_string) do
+      {:ok, date} -> date
+      _ -> nil
+    end
+  end
+
+  defp insert_practitioner(attrs) do
+    %Practitioner{}
+    |> Practitioner.changeset(attrs)
+    |> Repo.insert()
   end
 end
